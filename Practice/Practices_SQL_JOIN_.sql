9ed3dd885e0721a621347b2b5e566281916d7dfc
yildirimdata <yildirimkdata@gmail.com>
2023-01-19 17:57:15 +0100

    join more practices

--- Practice/Practices_SQL_JOIN_.sql
+++ Practice/Practices_SQL_JOIN_.sql
@@ -251,3 +251,19 @@ SELECT m.title, a.name
     ON c.actorid = a.id 
     WHERE c.ord = 1 
         AND m.yr = 1962;
+
+-- Q25
+-- Which were the busiest years for 'Rock Hudson', show the year and the number of movies 
+-- he made each year for any year in which he made more than 2 movies.
+
+SELECT yr,COUNT(title) 
+    FROM movie 
+    JOIN casting 
+        ON movie.id=movieid
+    JOIN actor   
+        ON actorid=actor.id
+    WHERE name='Rock Hudson'
+    GROUP BY yr
+    HAVING COUNT(title) > 2
+
+    
